8c07e0b53592a06566f17cd836ee93187d079a77
irriver <maraqann@gmail.com>
2020-07-22 00:01:17 +0900

    add file read method

--- src/gene/Part1.java
+++ src/gene/Part1.java
@@ -1,30 +1,43 @@
 package gene;
-import java.io.File;
 
-import edu.duke.DirectoryResource;
-import edu.duke.FileResource;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Part1 {
-	
-	String		findSimpleGene(String dna) {
-		String	gene = "";
-		int		startIndex = dna.indexOf("ATG");
+
+	static String findSimpleGene(String dna) {
+		String gene = "";
+		int startIndex = dna.indexOf("ATG");
 		if (startIndex == -1) {
-			return	"";
+			return "";
 		}
-		int		stopIndex = dna.indexOf("TAA", startIndex + 3); //find after start codon
+		int stopIndex = dna.indexOf("TAA", startIndex + 3); // find after start codon
 		if (stopIndex == -1) {
-			return	"";
+			return "";
 		}
 		gene = dna.substring(startIndex, stopIndex + 3);
 		if (gene.length() % 3 == 0) {
-			return	gene;
+			return gene;
 		}
 		return "no valid dna strands";
 	}
-	
+
 	public static void main(String[] args) {
-		findSimpleGene();
+		// File f = new File("C:\\Dev\\workSpace\\BlueJ\\dna\\mansmall.fa");
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader("C:\\Dev\\workSpace\\BlueJ\\dna\\mansmall.fa"));
+			String line = reader.readLine();
+			while (line != null) {
+				System.out.println(line);
+				// read next line
+				line = reader.readLine();
+				findSimpleGene(line);
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
 }
